5618966e752ac7720ae7ab329c8c42af7ebf3be9
wang <122589615@qq.com>
2019-07-20 08:06:54 +0800

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-WZMChatUI_Example/Pods-WZMChatUI_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WZMChatUI_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WZMChatUI_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-WZMChatUI_Tests/Pods-WZMChatUI_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WZMChatUI_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WZMChatUI_Tests
+@end
